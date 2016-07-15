51a248adbc6f7b737c0ea7da77c98ebed417878b
shenxy <shenxy@suilingzhi.deephi.nics.info>
2016-07-15 14:43:33 +0800

    Revert to vanilla

--- examples/imagenet/create_imagenet.sh
+++ examples/imagenet/create_imagenet.sh
@@ -2,16 +2,16 @@
 # Create the imagenet lmdb inputs
 # N.B. set the path to the imagenet train + val data dirs
 
-EXAMPLE=/opt/data/imagenet
+EXAMPLE=examples/imagenet
 DATA=data/ilsvrc12
 TOOLS=build/tools
 
-TRAIN_DATA_ROOT=/opt/data/imagenet/train/
-VAL_DATA_ROOT=/opt/data/imagenet/val/
+TRAIN_DATA_ROOT=/path/to/imagenet/train/
+VAL_DATA_ROOT=/path/to/imagenet/val/
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
 # already been resized using another tool.
-RESIZE=true
+RESIZE=false
 if $RESIZE; then
   RESIZE_HEIGHT=256
   RESIZE_WIDTH=256
